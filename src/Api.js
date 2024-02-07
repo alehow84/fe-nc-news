5efea2c3314ab8a7954a5b84fd87c5079087a640
Alexandra Howlett <alexandrahowlett@Alexandras-MacBook-Pro.local>
2024-02-07 14:48:42 +0000

    return single article page when an article card is clicked or existing article id entered in search bar

--- src/Api.js
+++ src/Api.js
@@ -7,3 +7,12 @@ export const getAllArticles = () => {
         return response.data
     })
 }   
+
+export const getSingleArticle = (article_id) => {
+    return axios
+    .get(`https://nc-news-dcnn.onrender.com/api/articles/${article_id}`)
+    .then((response)=>{
+        console.log(response.data, "response.data in singlearticle request")
+        return response.data
+    })
+}
--- src/Header.jsx
+++
@@ -1,6 +0,0 @@
-export default function Header(){
-
-    return (
-        <h1>Welcome to NC News 💬</h1>
-    )
-}
