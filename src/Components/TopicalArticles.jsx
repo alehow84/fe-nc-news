0b5f65dce40fda9682cda9c931784f7ecf7cb839
Alexandra Howlett <alexandrahowlett@Alexandras-MacBook-Pro.local>
2024-03-08 13:02:36 +0000

    render TopicalArticles component when an option is selected from Search Articles by Topic Form

--- src/Components/TopicalArticles.jsx
+++ src/Components/TopicalArticles.jsx
@@ -1,6 +1,6 @@
-export default function TopicalArticles(){
-    //this component will search for articles by Topic and render the articles card based on that response
-    return (
-        <h1>Articles by topic</h1>
-    )
+export default function TopicalArticles() {
+  //this component will search for articles by Topic and render the articles card based on that response
+  //use params hook - destructure topics and use this to make a call to api doing a get request for /api/articles?topic
+
+  return <h1>Articles by topic</h1>;
 }
--- src/Components/TopicsForm.jsx
+++ src/Components/TopicsForm.jsx
@@ -1,13 +1,16 @@
 import { Navigate, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function TopicsForm({ topics }) {
   const navigate = useNavigate();
   /*
     -create onChange event to grab the value of the selected option
+    -state needed to set selectedTopic
     -update the state of selectedTopic to the selected option
     */
   function handleTopicChange(event) {
     console.log(event.target.value, "<<event in handleTopicChange");
+    navigate(`/articles/topics/${event.target.value}`);
   }
 
   function handleResetFilter() {
@@ -19,16 +22,20 @@ export default function TopicsForm({ topics }) {
       <form onReset={handleResetFilter}>
         <label>Search Articles by Topic</label>
         <select defaultValue={"placeholder"} onChange={handleTopicChange}>
-          {/* {console.log(topics, "<<topics in TopicsOptions")} */}
-          {/* Sometimes I'm getting a typeError on the below, but when i first comment the code back in, it works. BC topics is initially set to an empty string */}
           <option disabled value={"placeholder"}>
             Choose a Topic
           </option>
           {topics.map((topic) => {
             return (
+              // <Link
+              //   to={`/articles/topics/${topic.slug}`}
+              //   id="topic"
+              //   key={topic.slug}
+              // >
               <option key={topic.slug} value={topic.slug}>
                 {topic.slug}
               </option>
+              // </Link>
             );
           })}
         </select>
